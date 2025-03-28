e607d853f37c7dee9b0c2c43835dff7f94db711d
JacobGeniesse <jacobgeniesse@gmail.com>
2025-03-28 18:03:58 -0400

    Enemy Replacements
    Created a new enemy, and projectile. Had enemy02 be made to no longer spawn due to the visual clutter it created.

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -6,11 +6,13 @@ public class gameManager : MonoBehaviour
 {
 public GameObject enemy01;
 public GameObject enemy02;
+public GameObject miniBoss;
 // Start is called before the first frame update
     void Start()
     {
      InvokeRepeating("Enemy01Spawn", 2.0f, 0.3f);
-     InvokeRepeating("Enemy02Spawn", 2.0f, 0.3f);
+     //InvokeRepeating("Enemy02Spawn", 2.0f, 0.3f);
+     InvokeRepeating("miniBossSpawn", 15.0f, 15.0f);
     }
 
    void Enemy01Spawn ()
@@ -19,9 +21,15 @@ public GameObject enemy02;
         Instantiate(enemy01, spawnpoint, Quaternion.identity);
     }
 
-    void Enemy02Spawn (){
-        var spawnpoint = new Vector3(11, Random.Range(-4.4f, 6.5f), 0f);
-        Instantiate(enemy02, spawnpoint, Quaternion.identity);
+    // Created Visual Clutter. Replaced with MiniBoss01
+   // void Enemy02Spawn (){
+   //     var spawnpoint = new Vector3(11, Random.Range(-4.4f, 6.5f), 0f);
+   //     Instantiate(enemy02, spawnpoint, Quaternion.identity);
+  //  }
+
+    void miniBossSpawn (){
+        var spawnpoint = new Vector3(0f, 6.0f, 0f);
+        Instantiate (miniBoss, spawnpoint, Quaternion.identity);
     }
     // Update is called once per frame
     void Update()
